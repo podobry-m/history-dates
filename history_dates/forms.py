31f0793fbc2d7f57e8693e082a2e68b01be46293
podobrii-ma <mpd@terraquantum.swiss>
2025-04-12 22:00:41 +0300

    Added validation

--- history_dates/forms.py
+++ history_dates/forms.py
@@ -1,10 +1,15 @@
 from django import forms
 from .models import HistoricalEvent
-
+from datetime import datetime
 class HistoricalEventForm(forms.ModelForm):
     class Meta:
         model = HistoricalEvent
         fields = ['date', 'title']
         widgets = {
-              'title': forms.Textarea(attrs={'style': "width:100%;"}),
+              'title': forms.Textarea(attrs={'style': "width:100%;", 'class': 'form-control'}),
+              'date': forms.NumberInput(attrs={
+                'class': 'form-control',
+                'min': 1,
+                'max': datetime.now().year
+            })
         }
--- history_dates/models.py
+++ history_dates/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from django.core.validators import MaxValueValidator
+from datetime import datetime
 
 class HistoricalEvent(models.Model):
-    title = models.TextField(blank=True, verbose_name="Событие")
-    date = models.IntegerField(verbose_name="Дата (год)")
+    title = models.TextField(verbose_name="Событие")
+    date = models.PositiveSmallIntegerField(verbose_name="Дата (год)", validators=[MaxValueValidator(datetime.now().year,'Ошибка, год не может быть больше текущего')])
     def __str__(self):
         return f"{self.date} - {self.title}"
 
